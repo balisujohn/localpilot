853c563400019f4566b75039f9e60f1e064cf9d7
John U. Balis <phobossystems@gmail.com>
2024-02-07 22:44:39 -0600

    updated to work with new text-generation-webui API

--- extension.js
+++ extension.js
@@ -13,6 +13,10 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "localpilot" is now active!');
+	
+	//unused but left because it's useful for debugging
+	//let localpilotLog = vscode.window.createOutputChannel("Localpilot Log");
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -24,42 +28,15 @@ function activate(context) {
 		let selectedText = editor.document.getText(editor.selection);
 	
 		let HOST = 'localhost:5000'
-		let URI = `http://${HOST}/api/v1/generate`
+		let URI = `http://${HOST}/v1/completions`
 
 		//derived from https://github.com/oobabooga/text-generation-webui/blob/main/api-examples/api-example.py
 		const request = {
 			'prompt': selectedText,
-			'max_new_tokens': 10,
-			'auto_max_new_tokens': false,
-			'preset': 'None',
-			'do_sample': true,
-			'temperature': 0.1,
-			'top_p': 0.1,
-			'typical_p': 1,
-			'epsilon_cutoff': 0,  
-			'eta_cutoff': 0,
-			'tfs': 1,
-			'top_a': 0,
-			'repetition_penalty': 1.18,
-			'repetition_penalty_range': 0,
-			'top_k': 40,
-			'min_length': 0,
-			'no_repeat_ngram_size': 0,
-			'num_beams': 1,
-			'penalty_alpha': 0,
-			'length_penalty': 1,
-			'early_stopping': false,
-			'mirostat_mode': 0,
-			'mirostat_tau': 5,
-			'mirostat_eta': 0.1,
-			'guidance_scale': 1,
-			'negative_prompt': '',
-			'seed': -1,
-			'add_bos_token': true,
-			'truncation_length': 2048,
-			'ban_eos_token': false,
-			'skip_special_tokens': true,
-			'stopping_strings': []
+			"max_tokens": 10,
+			"temperature": 1,
+			"top_p": 0.9,
+			"seed": 10
 		}
 		
 		fetch(URI, {
@@ -71,7 +48,8 @@ function activate(context) {
 		})
 		.then(response => response.json())
 		.then(data => {
-			let resultText = data['results'][0]['text'];
+			let resultText = data.choices[0].text;
+
 			vscode.window.activeTextEditor.edit(edit => {
 				edit.insert(editor.selection.end, resultText)
 			});
@@ -94,13 +72,12 @@ function activate(context) {
 		let selectedText = editor.document.getText(editor.selection);
 	
 		let HOST = 'localhost:5000'
-		let URI = `http://${HOST}/api/v1/generate`
+		let URI = `http://${HOST}/v1/completions`
 
 		let options = {
 			prompt: "Enter Edit Instructions", 
 			placeHolder: "",
 		}
-
 
 		vscode.window.showInputBox(options).then(response =>
 		{
@@ -109,37 +86,10 @@ function activate(context) {
 				//derived from https://github.com/oobabooga/text-generation-webui/blob/main/api-examples/api-example.py
 		const request = {
 			'prompt': "ORIGINAL VERSION:\n" + selectedText + "\n Below is the above code according to the following instructions. Don't add comments.: \n"+response+"\nEDITED VERSION:\n",
-			'max_new_tokens': selectedText.length,
-			'auto_max_new_tokens': false,
-			'preset': 'None',
-			'do_sample': true,
-			'temperature': 0.1,
-			'top_p': 0.1,
-			'typical_p': 1,
-			'epsilon_cutoff': 0,  
-			'eta_cutoff': 0,
-			'tfs': 1,
-			'top_a': 0,
-			'repetition_penalty': 1.18,
-			'repetition_penalty_range': 0,
-			'top_k': 40,
-			'min_length': 0,
-			'no_repeat_ngram_size': 0,
-			'num_beams': 1,
-			'penalty_alpha': 0,
-			'length_penalty': 1,
-			'early_stopping': false,
-			'mirostat_mode': 0,
-			'mirostat_tau': 5,
-			'mirostat_eta': 0.1,
-			'guidance_scale': 1,
-			'negative_prompt': '',
-			'seed': -1,
-			'add_bos_token': true,
-			'truncation_length': 2048,
-			'ban_eos_token': false,
-			'skip_special_tokens': true,
-			'stopping_strings': []
+			'max_tokens': selectedText.length,
+			"temperature": 1,
+			"top_p": 0.9,
+			"seed": 10
 		}
 		
 		fetch(URI, {
@@ -151,7 +101,7 @@ function activate(context) {
 		})
 		.then(response => response.json())
 		.then(data => {
-			let resultText = data['results'][0]['text'];
+			let resultText = data.choices[0].text
 			vscode.window.activeTextEditor.edit(edit => {
 				edit.replace(editor.selection, resultText)
 			});
